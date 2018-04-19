ea900da33131c1a90c473181c7bffcf2ede41e1d
Joshua Whitley <jwhitley@autonomoustuff.com>
2018-04-19 15:13:09 -0500

    Adding updated/fixed Travis files.

---
+++ trusty_install_script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+wget https://s3.us-east-2.amazonaws.com/astuff-common-lib/ubuntu_1404/libas-common_2.0.0-0~14.040_amd64.deb -O /tmp/libas-common.deb
+dpkg -i /tmp/libas-common.deb
+apt-get install software-properties-common -y
+add-apt-repository ppa:ubuntu-toolchain-r/test -y
+apt-get update -qq
+apt-get install gcc-5 g++-5 -y
+update-alternatives --install /usr/bin/gcc gcc /usr/bin/gcc-5 1 --slave /usr/bin/g++ g++ /usr/bin/g++-5
